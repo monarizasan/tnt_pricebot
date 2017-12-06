cb3d577b3a8bf85b108ac20d4900b0758bccf615
monarizasan <32188449+monarizasan@users.noreply.github.com>
2017-12-06 12:18:16 +0900

    Update tnt_pricebot.py

--- tnt_pricebot.py
+++ tnt_pricebot.py
@@ -18,19 +18,34 @@ def get_tntprice():
     r_liqui = requests.get('https://api.liqui.io/api/3/ticker/tnt_btc')
     json_liqui = r_liqui.json()
     liqui_sat = int(json_liqui['tnt_btc']['last']*10**8)
-    liqui = 'Liqui:  ' + "%.8f" % float(liqui_sat/10**8) + ' BTC'
+    
+    r_liqui_usdt = requests.get('https://api.liqui.io/api/3/ticker/btc_usdt')
+    json_liqui_usdt = r_liqui_usdt.json()
+    liqui_usdt = "%.3f" % float(float(json_liqui_usdt['btc_usdt']['last'])*float(json_liqui['tnt_btc']['last']))
+    
+    liqui = 'Liqui:  ' + "%.8f" % float(liqui_sat/10**8) + ' BTC' + ' ($' + liqui_usdt + ')'
 
     #binance
     r_binance = requests.get('https://api.binance.com//api/v1/ticker/24hr?symbol=TNTBTC')
     json_binance = r_binance.json()
     binance_sat = int(float(json_binance['lastPrice'])*10**8)
-    binance = 'Binance:' + "%.8f" % float(binance_sat/10**8) + ' BTC'
+    
+    r_binance_usdt = requests.get('https://api.binance.com/api/v1/ticker/24hr?symbol=BTCUSDT')
+    json_binance_usdt = r_binance_usdt.json()
+    binance_usdt = "%.3f" % float(float(json_binance['lastPrice'])*float(json_binance_usdt['lastPrice']))
+    
+    binance = 'Binance:' + "%.8f" % float(binance_sat/10**8) + ' BTC' + ' ($' + binance_usdt + ')'
 
     #huobi
     r_huobi = requests.get('https://api.huobi.pro/market/trade?symbol=tntbtc')
     json_huobi = r_huobi.json()
     huobi_sat = int(float(json_huobi['tick']['data'][0]['price']*10**8))
-    huobi = 'Huobi:  ' + "%.8f" % float(huobi_sat/10**8) + ' BTC'
+    
+    r_huobi_usdt = requests.get('https://api.huobi.pro/market/trade?symbol=btcusdt')
+    json_huobi_usdt = r_huobi_usdt.json()
+    huobi_usdt = "%.3f" % float(float(json_huobi_usdt['tick']['data'][0]['price'])*float(json_huobi['tick']['data'][0]['price']))
+    
+    huobi = 'Huobi:  ' + "%.8f" % float(huobi_sat/10**8) + ' BTC' + ' ($' + huobi_usdt + ')'
     
     tweet = 'Current prices of TNT are' + '\n' + '\n' + liqui + '\n' + binance + '\n' + huobi + '\n' + '\n' + '$TNT #Tierion'
     
